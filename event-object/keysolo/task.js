a29a2c90f3016b4f97fde76c8fc889df04acb11e
Собаева Екатерина <kat9228@yandex.ru>
2022-08-21 13:57:09 +0300

    task done

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,13 +17,16 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
+    document.addEventListener('keydown', (e) => {
+        const inputSymbol = e.key.toLowerCase();
+        const symbol = this.currentSymbol.textContent.toLowerCase();
+
+        if (inputSymbol === symbol) {
+          this.success();
+        } else {
+          this.fail();
+        }
+    })
   }
 
   success() {
